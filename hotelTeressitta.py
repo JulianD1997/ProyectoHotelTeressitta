eca0ca4ba6045fe0ac6408ceb7a38ae573faa3a8
Julian Casallas <julian.casallasb@gmail.com>
2022-06-05 19:24:35 -0300

    Se declaran las funciones para el CRUD

--- hotelTeressitta.py
+++ hotelTeressitta.py
@@ -2,27 +2,70 @@ import sqlite3
 from tkinter import *
 from tkinter import ttk
 from tkcalendar import DateEntry
+from datetime import datetime
+import traceback
 
 # funciones
-def conexionSQL():
+def conexionSQL(consulta, parametros=()):
     miConexion = sqlite3.connect("HotelTeressitta")
-    cursor= miConexion.cursor()
+    cursor = miConexion.cursor()
     try:
-        cursor.execute('''--sql
-            CREATE TABLE Clientes(
-                ID  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                nombre VARCHAR(75),
-                apellido VARCHAR(75)
-                DNI INTEGER(15) UNIQUE NOT NULL,
-                habitacion INTEGER(6) UNIQUE NOT NULL,
-                fechaDeIngreso DATE NOT NULL,
-                fechaDESalida DATE NOT NULL,
-            );'''
-        )
-    except:
-        print("La tabla ya existe")
+        cursor.execute(consulta, parametros)
+    except Exception:
+        traceback.print_exc()
     finally:
         miConexion.commit()
+
+def crearTabla():
+    consulta = ('''--sql
+        CREATE TABLE Clientes(
+            ID  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            nombre VARCHAR(75),
+            apellido VARCHAR(75),
+            DNI INTEGER(15) UNIQUE NOT NULL,
+            habitacion INTEGER(6) UNIQUE NOT NULL,
+            fechaDeIngreso DATE NOT NULL,
+            fechaDESalida DATE NOT NULL
+        );'''
+                )
+    conexionSQL(consulta)
+
+def crearCliente():
+    consulta = """--sql
+        INSERT INTO Clientes
+        values(NULL,?,?,?,?,?,?)
+    ;
+    """
+    ingreso = datetime.strptime(fechaIngreso.get(), "%d/%m/%y").date()
+    salida = datetime.strptime(fechaIngreso.get(), "%d/%m/%y").date()
+
+    parametros = (nombre.get(), apellido.get(), dni.get(), habitacion.get(),ingreso,salida)
+    conexionSQL(consulta,parametros)
+
+def modificarCliente():
+    consulta = """--sql
+        UPDATE Clientes
+        SET nombre = ?,
+            apellido = ?, 
+            DNI = ?,
+            habitacion = ?,
+            fechaDeIngreso = ?,
+            fechaDeSalida = ?,
+        where DNI= ?;
+    """
+    ingreso = datetime.strptime(fechaIngreso.get(), "%d/%m/%y").date()
+    salida = datetime.strptime(fechaIngreso.get(), "%d/%m/%y").date()
+
+    parametros = (nombre.get(), apellido.get(), dni.get(), habitacion.get(),ingreso,salida)
+    conexionSQL(consulta,parametros)
+
+def borrarCliente():
+    consulta= """--sql
+        DELETE FROM Clientes
+        WHERE DNI=?;
+    """
+    parametros=()
+    conexionSQL(consulta, parametros) 
 
 def setearForms():
     nombre.set("")
@@ -33,17 +76,13 @@ def setearForms():
     fechaSalida.set("//")
 
 def accionBoton():
-    if (botonVariable.get()=="Guardar"):
-        crear()
+    if (botonVariable.get() == "Guardar"):
+        crearCliente()
     else:
-        print("print)")
+        modificarCliente()
 
     botonVariable.set("Guardar")
     setearForms()
-
-def crear():
-    botonVariable.set("Guardar")
-    print("Nombre :",nombre.get(),"Apellido :",apellido.get(),"DNI :",dni.get(),"Habitacion :",habitacion.get(),"Fecha de Ingreso :",fechaIngreso.get(),"Fecha de Salida :",fechaSalida.get())
 
 def actualizar():
     botonVariable.set("Actualizar")
@@ -57,97 +96,107 @@ def actualizar():
 root = Tk()
 root.title("Hotel Teressitta")
 
-#Variables
-nombre=StringVar()
-apellido=StringVar()
-dni=IntVar()
-habitacion=IntVar()
-fechaIngreso=StringVar()
-fechaSalida=StringVar()
-botonVariable=StringVar()
+# Variables
+crearTabla()
+nombre = StringVar()
+apellido = StringVar()
+dni = IntVar()
+habitacion = IntVar()
+fechaIngreso = StringVar()
+fechaSalida = StringVar()
+botonVariable = StringVar()
 botonVariable.set("Guardar")
 
-#Se declaran los Frames
+# Se declaran los Frames
 marco = ttk.Frame(root, padding=10)
-formulario = ttk.LabelFrame(marco,height=700,text="Formulario cliente",padding=10)
-listaDatos = ttk.LabelFrame(marco,height=700,text="Lista de clientes",padding=10)
-herramientas = ttk.LabelFrame(marco,height=700,text="Herramientas",padding=10)
+formulario = ttk.LabelFrame(
+    marco, height=700, text="Formulario cliente", padding=10)
+listaDatos = ttk.LabelFrame(
+    marco, height=700, text="Lista de clientes", padding=10)
+herramientas = ttk.LabelFrame(
+    marco, height=700, text="Herramientas", padding=10)
 
-#formularios
-formularioNombre = ttk.Entry(formulario,textvariable=nombre)
-formularioApellido = ttk.Entry(formulario,textvariable=apellido)
-formularioDNI =ttk.Entry(formulario,textvariable=dni)
-formularioHabitacion = ttk.Entry(formulario,textvariable=habitacion)
-formularioFechaIngreso = DateEntry(formulario,selectmode="dia",textvariable=fechaIngreso)
-formularioFechaSalida = DateEntry(formulario,selectmode="dia",textvariable=fechaSalida)
+# formularios
+formularioNombre = ttk.Entry(formulario, textvariable=nombre)
+formularioApellido = ttk.Entry(formulario, textvariable=apellido)
+formularioDNI = ttk.Entry(formulario, textvariable=dni)
+formularioHabitacion = ttk.Entry(formulario, textvariable=habitacion)
+formularioFechaIngreso = DateEntry(
+    formulario, selectmode="dia", textvariable=fechaIngreso)
+formularioFechaSalida = DateEntry(
+    formulario, selectmode="dia", textvariable=fechaSalida)
 
-#botones
-botonAccion=ttk.Button(formulario,textvariable=botonVariable,padding= ("10 5 10 5"),command=accionBoton)
-botonCrear=ttk.Button(herramientas,text="Crear",padding= ("10 5 10 5"),)
-botonActualizar= ttk.Button(herramientas,text="Actualizar",padding= ("10 5 10 5"),command=actualizar)
-botonEliminar= ttk.Button(herramientas,text="Eliminar",padding= ("10 5 10 5"))
+# botones
+botonAccion = ttk.Button(formulario, textvariable=botonVariable, padding=(
+    "10 5 10 5"), command=accionBoton)
+botonCrear = ttk.Button(herramientas, text="Crear", padding=("10 5 10 5"))
+botonActualizar = ttk.Button(
+    herramientas, text="Actualizar", padding=("10 5 10 5"))
+botonEliminar = ttk.Button(
+    herramientas, text="Eliminar", padding=("10 5 10 5"))
 
 # etiquetas
-etiquetaNombre = ttk.Label(formulario,text="Nombre")
-etiquetaApellido = ttk.Label(formulario,text="Apellido")
-etiquetaDNI = ttk.Label(formulario,text="DNI")
-etiquetaHabitacion = ttk.Label(formulario,text="Habitacion")
-etiquetaFechaIngreso = ttk.Label(formulario,text="Fecha de Ingreso")
+etiquetaNombre = ttk.Label(formulario, text="Nombre")
+etiquetaApellido = ttk.Label(formulario, text="Apellido")
+etiquetaDNI = ttk.Label(formulario, text="DNI")
+etiquetaHabitacion = ttk.Label(formulario, text="Habitacion")
+etiquetaFechaIngreso = ttk.Label(formulario, text="Fecha de Ingreso")
 etiquetaFechaSalida = ttk.Label(formulario, text="Fechas de salida")
 
 # se empaquetan los elementos
-marco.grid(column=0, row=0,sticky=(N,S,E,W))
+marco.grid(column=0, row=0, sticky=(N, S, E, W))
 formulario.grid(column=0, row=0)
-listaDatos.grid(column=0, row=1,pady=5)
+listaDatos.grid(column=0, row=1, pady=5)
 herramientas.grid(column=0, row=2)
 
-etiquetaNombre.grid(column=0, row =0,sticky=SW, padx=5)
+etiquetaNombre.grid(column=0, row=0, sticky=SW, padx=5)
 formularioNombre.grid(column=0, row=1, sticky=(W), padx=5)
 
-etiquetaApellido.grid(column=1, row =0, sticky=W, padx=5)
-formularioApellido.grid(column=1, row=1, sticky=(W),padx=5)
+etiquetaApellido.grid(column=1, row=0, sticky=W, padx=5)
+formularioApellido.grid(column=1, row=1, sticky=(W), padx=5)
 
-etiquetaDNI.grid(column=2, row =0,sticky=W,padx=5)
-formularioDNI.grid(column=2, row=1, sticky=(W),padx=5)
+etiquetaDNI.grid(column=2, row=0, sticky=W, padx=5)
+formularioDNI.grid(column=2, row=1, sticky=(W), padx=5)
 
-etiquetaHabitacion.grid(column=3, row =0,sticky=W,padx=5)
-formularioHabitacion.grid(column=3, row=1,sticky=(W), padx=5)
+etiquetaHabitacion.grid(column=3, row=0, sticky=W, padx=5)
+formularioHabitacion.grid(column=3, row=1, sticky=(W), padx=5)
 
-etiquetaFechaIngreso.grid(column=4, row =0,sticky=W,padx=5)
-formularioFechaIngreso.grid(column=4, row=1,sticky=(W), padx=5)
+etiquetaFechaIngreso.grid(column=4, row=0, sticky=W, padx=5)
+formularioFechaIngreso.grid(column=4, row=1, sticky=(W), padx=5)
 
-etiquetaFechaSalida.grid(column=5, row =0,sticky=W,padx=5)
-formularioFechaSalida.grid(column=5, row=1,sticky=(W),padx=5)
+etiquetaFechaSalida.grid(column=5, row=0, sticky=W, padx=5)
+formularioFechaSalida.grid(column=5, row=1, sticky=(W), padx=5)
 
-botonAccion.grid(column=5,row=2,sticky=W,pady=10)
-botonCrear.grid(column=0,row=0,sticky=W,padx=20)
-botonActualizar.grid(column=1, row=0,sticky=W,padx=20)
-botonEliminar.grid(column=2,row=0,sticky=W,padx=20)
+botonAccion.grid(column=5, row=2, sticky=W, pady=10)
+botonCrear.grid(column=0, row=0, sticky=W, padx=20)
+botonActualizar.grid(column=1, row=0, sticky=W, padx=20)
+botonEliminar.grid(column=2, row=0, sticky=W, padx=20)
 
 # Lista de clientes
-arbol=ttk.Treeview(listaDatos,columns=6,)
-arbol['columns'] =('nombre','apellido','DNI','habitacion','fecha ingreso','fecha salida')
+arbol = ttk.Treeview(listaDatos, columns=6,)
+arbol['columns'] = ('nombre', 'apellido', 'DNI',
+                    'habitacion', 'fecha ingreso', 'fecha salida')
 
-arbol.grid(column=0,row=0)
-arbol.column('#0',width=40,minwidth=10)
-arbol.heading("#0",text='No')
+arbol.grid(column=0, row=0)
+arbol.column('#0', width=40, minwidth=10)
+arbol.heading("#0", text='No')
 
-arbol.column('nombre',width=120,minwidth=10)
-arbol.heading('nombre',text='Nombre')
+arbol.column('nombre', width=120, minwidth=10)
+arbol.heading('nombre', text='Nombre')
 
-arbol.column('apellido',width=120,minwidth=10)
-arbol.heading('apellido',text='Apellido')
+arbol.column('apellido', width=120, minwidth=10)
+arbol.heading('apellido', text='Apellido')
 
-arbol.column('DNI',width=100,minwidth=10)
-arbol.heading('DNI',text='DNI')
+arbol.column('DNI', width=100, minwidth=10)
+arbol.heading('DNI', text='DNI')
 
-arbol.column('habitacion',width=100,minwidth=10)
-arbol.heading('habitacion',text='Habitacion')
+arbol.column('habitacion', width=100, minwidth=10)
+arbol.heading('habitacion', text='Habitacion')
 
-arbol.column('fecha ingreso',width=100,minwidth=10)
-arbol.heading('fecha ingreso',text='Fecha ingreso')
+arbol.column('fecha ingreso', width=100, minwidth=10)
+arbol.heading('fecha ingreso', text='Fecha ingreso')
 
-arbol.column('fecha salida',width=100,minwidth=10)
-arbol.heading('fecha salida',text='Fecha Salida')
+arbol.column('fecha salida', width=100, minwidth=10)
+arbol.heading('fecha salida', text='Fecha Salida')
 
 root.mainloop()
